5ef1454a568b33f4a71a42b8de7d54f5dd5d1275
marwajomaa <marwajomaa4@gmail.com>
2020-12-27 22:30:51 +0200

    get payments

--- server/controllers/paymentCtrl.js
+++ server/controllers/paymentCtrl.js
@@ -3,22 +3,8 @@ const { getUserById } = require("../queries/users");
 
 exports.getPayment = async (req, res, next) => {
   try {
-    const user = await getUserById(req.user.userId).select("name email");
-    if (!user) return res.status(400).json({ msg: "User does not exist" });
-
-    const { cart, paymentID, address } = req.body;
-    const { _id, name, email } = user;
-
-    const newPayment = new Payments({
-      user_id: _id,
-      name,
-      email,
-      cart,
-      paymentID,
-      address,
-    });
-
-    res.json({ status: "success", data: newPayment });
+    const payments = await Payments.find();
+    res.json(payments);
   } catch (err) {
     return res.status(500).json({ msg: err.message });
   }
