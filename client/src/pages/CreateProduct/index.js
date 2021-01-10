815ea18d9b4de1aef18ea2df1ead02373eafbdaa
marwajomaa <marwajomaa4@gmail.com>
2021-01-11 00:32:22 +0200

    fix form style

--- client/src/pages/CreateProduct/index.js
+++ client/src/pages/CreateProduct/index.js
@@ -15,6 +15,10 @@ const useStyles = makeStyles(() => ({
     alignItems: "center",
     textAlign: "center",
   },
+  paragraph: {
+    margin: "10px 0",
+    padding: "10px",
+  },
 }));
 
 const initialValues = {
@@ -25,12 +29,6 @@ const initialValues = {
   content: "",
   category: "",
 };
-
-const genderItems = [
-  { id: "male", title: "Male" },
-  { id: "female", title: "Female" },
-  { id: "other", title: "Other" },
-];
 
 function CreateProduct() {
   const state = useContext(GlobalState);
@@ -59,51 +57,65 @@ function CreateProduct() {
   return (
     <Paper elevation={0}>
       <form className={classes.container} onSubmit={handleSubmit}>
-        <Typography variant="h6" component="p">
+        <Typography variant="h6" component="p" className={classes.paragraph}>
           Create New Product
         </Typography>
-        <Input
-          label="Title"
-          type="text"
-          name="title"
-          value={values.title}
-          onChange={handleInputChange}
-        />
-        <Input
-          label="Price"
-          type="number"
-          name="price"
-          value={values.price}
-          onChange={handleInputChange}
-        />
-        <Input
-          label="Description"
-          type="text"
-          name="description"
-          value={values.description}
-          onChange={handleInputChange}
-        />
-        <Input
-          label="Content"
-          type="text"
-          name="content"
-          value={values.content}
-          onChange={handleInputChange}
-        />
-        <Select
-          name="category"
-          label="Category"
-          value={values.category}
-          onChange={handleInputChange}
-          options={categories && categories}
-        />
-        <Button
-          variant="contained"
-          color="primary"
-          text="Submit"
-          type="submit"
-          style={{ width: "100%" }}
-        />
+        <Grid container direction="column" spacing={2}>
+          <Grid item xs={12}>
+            <Input
+              label="Title"
+              type="text"
+              name="title"
+              value={values.title}
+              onChange={handleInputChange}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Input
+              label="Price"
+              type="number"
+              name="price"
+              value={values.price}
+              onChange={handleInputChange}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Input
+              label="Description"
+              type="text"
+              name="description"
+              value={values.description}
+              onChange={handleInputChange}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Input
+              label="Content"
+              type="text"
+              name="content"
+              value={values.content}
+              onChange={handleInputChange}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Select
+              name="category"
+              label="Category"
+              value={values.category}
+              onChange={handleInputChange}
+              options={categories && categories}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              variant="contained"
+              color="primary"
+              text="Submit"
+              type="submit"
+              style={{ width: "100%" }}
+            />
+          </Grid>
+        </Grid>
       </form>
     </Paper>
   );
