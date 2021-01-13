fd04dce4e985498968a8732701284c08f23d5bff
marwajomaa <marwajomaa4@gmail.com>
2021-01-13 23:24:32 +0200

    checkbox for select all products

--- client/src/pages/Products/index.js
+++ client/src/pages/Products/index.js
@@ -1,12 +1,14 @@
-import { Typography, Grid } from "@material-ui/core";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import { Typography, Grid, Checkbox } from "@material-ui/core";
 import { GlobalState } from "../../GlobalState";
 import ProductItem from "./ProductItem";
+import Button from "../../components/Button";
 import Loading from "../../components/Loading";
 
 function Products() {
   const state = useContext(GlobalState);
   const [products, setProducts] = state.productsAPI.products;
+  const [isCheck, setIsCheck] = useState(false);
   if (products.length === 0) return <Loading />;
 
   const handleCheck = (id) => {
@@ -15,8 +17,22 @@ function Products() {
     });
     setProducts([...products]);
   };
+
+  const checkAll = () => {
+    products.forEach((product) => (product.checked = !product.checked));
+    setProducts([...products]);
+    setIsCheck(true);
+  };
+
   return (
     <Grid container spacing={3}>
+      <Grid>
+        <Typography variant="h6" component="span">
+          Check All
+        </Typography>
+        <Checkbox checked={isCheck} onChange={checkAll} />
+        <Button text="Delete All" />
+      </Grid>
       {products &&
         products.map((product) => {
           return (
