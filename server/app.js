92cbe71e8d85d8ddded59d03090a03ac60847836
marwajomaa <marwajomaa4@gmail.com>
2021-02-05 22:31:20 +0200

    test1

--- server/app.js
+++ server/app.js
@@ -11,9 +11,9 @@ const path = require("path");
 const app = express();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client", "build")));
+  app.use(express.static(path.join(__dirname, "..", "client", "build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
   });
 }
 
